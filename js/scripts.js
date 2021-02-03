60db6f1964a7f12bdda5785781618a46204f957c
geomio <geoff.millner22@gmail.com>
2021-02-03 10:51:46 -0800

    added multiple function, fixed tags and made readme,

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,34 @@ function add(number1, number2) {
   return number1 + number2;
 }
 
-const number1 = parseInt(prompt("Enter a number:"));
+const number1 = parseInt(prompt("+Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 const result = add(number1, number2);
 alert(result);
+
+function divide(number4, number3) {
+  return number4 / number3;
+}
+
+const number4 = parseInt(prompt("/Enter a number:"));
+const number3 = parseInt(prompt("Enter another number:"));
+const result2 = divide(number4, number3);
+alert(result2);
+
+function multiply(number5, number6) {
+  return number5 * number6;
+}
+
+const number5 = parseInt(prompt("*Enter a number:"));
+const number6 = parseInt(prompt("Enter another number:"));
+const result3 = multiply(number5, number6);
+alert(result3);
+
+function subtract(number7, number8) {
+  return number7 - number8;
+}
+
+const number7 = parseInt(prompt("-Enter a number:"));
+const number8 = parseInt(prompt("Enter another number:"));
+const result4 = subtract(number7, number8);
+alert(result4);
---
+++ readme.md
@@ -0,0 +1,66 @@
+
+# _{Simple Js Calculator}_
+
+  
+
+#### _{Simple test calculator app using html javascript and css}, {2/3/2021 current version}_
+
+  
+
+#### By _**{geoff millner}**_
+
+  
+
+## Description
+
+  
+
+_{This is a simple calculator app so far it can add divide multiply and subtract. }_
+
+  
+
+## Setup/Installation Requirements
+
+  
+
+*  _go to this link at git hub_
+
+*  _open git bash, or other terminal_
+
+* _excluude $ it may be diffrent than yours type this below
+
+*_git clone www.adress.com/fiddledidlke_
+
+*  _open with your choice of browser_
+
+*  _please comment or message me on git bash_
+
+  
+  
+
+## Known Bugs
+
+  
+
+_{May crash if words are entered in numbers place}_
+
+  
+
+## Support and contact details
+
+  
+
+_{contact me via}_
+
+  
+
+## Technologies Used
+
+  
+
+_{Made using java script, html, and css, text editor used Visual Studio Code.}_
+
+  
+  
+
+Copyright (c) 2020 **_{Geoff Millner}_**
